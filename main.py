c8775ef7841e3cbbba81deee414d8347dbd00ff3
luis alejandro mora <luismoraj641@gmail.com>
2022-03-10 19:38:51 -0500

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from distutils.log import debug
 from flask import Flask, render_template, request,redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-import random
-import string
-import os
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '',
+    database = 'acortador',
+    port = 3306
+)
+
+db.autocommit = True
 
 
 app = Flask(__name__)
